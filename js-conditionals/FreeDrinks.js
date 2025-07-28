203073b2b612bc0058e8ac6e75e4c595a72e4e55
kaisarzafrin07 <zafrinkaisar07@gmail.com>
2025-07-28 15:32:27 +0600

    free drinks code updated

---
+++ js-conditionals/FreeDrinks.js
@@ -0,0 +1,32 @@
+/***
+
+Free Drinks
+    - Burger more than 500tk: free Coke
+    - Else Coke: 30tk
+*/
+
+let burgerPrice = 700;  // Example price of the burger
+
+//decision making
+if (burgerPrice > 500) {
+    console.log("Free Coke with your burger!");
+}   
+
+else {
+    console.log("Coke will cost you 30tk.");
+}
+
+let pizzaPrice = 800;  // Example price of the pizza
+//decision making for pizza
+if (pizzaPrice > 500) {
+    console.log("Free Coke with your pizza!");
+}
+
+let pastaPrice = 250;  // Example price of the pasta
+//decision making for pasta
+if(pastaPrice>300){
+    console.log("Free Coke with your pasta!");  
+}
+else {
+    console.log(" You have to buy coke with your pasta, Coke will cost you 30tk.");
+}
