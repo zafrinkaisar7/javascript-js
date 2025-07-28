172b0f9bc4b0ae9b4516ad97e30c85629bae2ca2
kaisarzafrin07 <zafrinkaisar07@gmail.com>
2025-07-28 14:52:16 +0600

    first commit

---
+++ practice1.js
@@ -0,0 +1,9 @@
+// Given Input values
+let moneyIhave = 1000;
+let costOfOrangesAndApples = 700;
+
+// Calculate the remaining money after buying oranges and apples
+let remainingMoney = moneyIhave - costOfOrangesAndApples;
+
+//Output for the result
+console.log( remainingMoney);
