c57111c7e7f59fefd5f38c6e566e3fa53942e51b
kaisarzafrin07 <zafrinkaisar07@gmail.com>
2025-07-28 14:57:16 +0600

    practice task codes added with problems

--- practice1.js
+++ practice1.js
@@ -1,3 +1,21 @@
+/*Task-1 Question:
+You went to the supermarket to buy some oranges and apples. Calculate how much money the shopkeeper will return.
+
+Input:
+The first line of the input is the taka you have. The second line is the cost of 1 kg of oranges and 1 kg of apples.
+
+Output:
+Print the result.
+
+Sample Input:
+1000
+700
+
+Sample Output:
+300
+*/
+
+
 // Given Input values
 let moneyIhave = 1000;
 let costOfOrangesAndApples = 700;
