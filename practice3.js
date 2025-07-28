5d73ee2df680a340a49679d5148ef9c7ff53c30c
kaisarzafrin07 <zafrinkaisar07@gmail.com>
2025-07-28 15:07:55 +0600

    task 3 solved with codes

---
+++ practice3.js
@@ -0,0 +1,20 @@
+/*  Task-3
+You task is to divide the given number by 5 and show the remainder as the output.
+
+Input:
+The first line of the input contains the number.
+
+Output:
+Print the remainder.
+
+Sample Input:
+119
+
+Sample Output:
+4 */
+
+let inputNumber = 119;
+
+let remainder = inputNumber % 5;
+
+console.log(remainder);
